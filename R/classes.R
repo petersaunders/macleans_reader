a399ecda38a04531ba58e66065010ca256a12405
Peter Saunders <peter.saunders@rubikloud.com>
2016-07-06 19:40:29 -0400

    Initial commit of R S4 classes

---
+++ R/classes.R
@@ -0,0 +1,22 @@
+# Objects for reading/writing Macleans articles
+
+#' RSS Channel Object
+MacleansRSSChannel <- setClass("MacleansRSSChannel",
+                                slots = list(name    = "character",
+                                             title   = "character",
+                                             url     = "character")
+                                )
+
+#' Magazine Article / Page Object
+MacleansArticle <- setClass("MacleansArticle",
+                            slots = list(title           = "character",
+                                         description     = "character",
+                                         content         = "character",
+                                         creator         = "character",
+                                         pubdate         = "POSIXct",
+                                         categories      = "character",
+                                         url             = "character",
+                                         guid            = "character",
+                                         comment_url     = "character",
+                                         comment_count   = "integer"
+                    ))
